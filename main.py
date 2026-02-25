f9801147dd26b12720ffd2c073621027d8ac4a05
Sponge eater <thomastaylorcoeli@gmail.com>
2026-02-26 07:58:11 +0800

    Refactor main.py to improve code structure

--- main.py
+++ main.py
@@ -78,11 +78,11 @@ def teams(e):
         display('Please select both registration and medical status.', target='output')
         return
 
-    if regis == "yes" and medi == "yes" and grade in ["7", "8", "9", "10"]:
+    if registration == "yes" and clearance == "yes" and grade in ["7", "8", "9", "10"]:
         eligible = "Yes"
-    if regis == "no" and medi == "yes" and grade in ["7", "8", "9", "10"]:
+    if registration == "no" and clearance == "yes" and grade in ["7", "8", "9", "10"]:
         display (f'register online first', target='output')
-    if regis == "yes" and medi == "no" and grade in ["7", "8", "9", "10"]:
+    if registration == "yes" and clearance == "no" and grade in ["7", "8", "9", "10"]:
         display (f'clearance is required', target='output')
     else:
         eligible = "You are not eligible for registration."
@@ -116,6 +116,7 @@ def teams(e):
         document.getElementById("output1").innerHTML = "<img src='y.jpeg' alt='Yellow Team' height='40%' width='40%'>"
     elif team == "none":
         display(f'Sorry, but you are not registered.', target='output')
+
 
 
 
